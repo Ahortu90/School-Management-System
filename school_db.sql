5f9a67c3591c3e2ffb620e8fa9c87da813bc904c
Ahortu90 <ahortuderrick4@gmail.com>
2022-09-26 01:35:23 +0000

    updates

--- school_db.sql
+++ school_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2022 at 11:16 PM
+-- Generation Time: Sep 26, 2022 at 03:29 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `school_db`
 --
+CREATE DATABASE IF NOT EXISTS `school_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `school_db`;
 
 -- --------------------------------------------------------
 
@@ -27,27 +29,61 @@ SET time_zone = "+00:00";
 -- Table structure for table `attendance`
 --
 
-CREATE TABLE `attendance` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `attendance` (
   `registration_number` varchar(20) NOT NULL,
   `full_name` varchar(100) NOT NULL,
   `Class` varchar(20) NOT NULL,
-  `date` varchar(20) NOT NULL
+  `date` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Truncate table before insert `attendance`
+--
+
+TRUNCATE TABLE `attendance`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `feeding_fees`
 --
 
-CREATE TABLE `feeding_fees` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `feeding_fees` (
   `full_name` varchar(100) NOT NULL,
   `Class` varchar(20) NOT NULL,
   `amount_paid` varchar(20) NOT NULL,
   `date` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Truncate table before insert `feeding_fees`
+--
+
+TRUNCATE TABLE `feeding_fees`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_info`
+--
+
+CREATE TABLE IF NOT EXISTS `login_info` (
+  `username` varchar(20) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `confirm_password` varchar(100) NOT NULL,
+  UNIQUE KEY `username` (`username`,`fullname`,`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Truncate table before insert `login_info`
+--
+
+TRUNCATE TABLE `login_info`;
+--
+-- Dumping data for table `login_info`
+--
+
+INSERT DELAYED IGNORE INTO `login_info` (`username`, `fullname`, `password`, `confirm_password`) VALUES
+('as', 'ahortu', '12', '12');
 
 -- --------------------------------------------------------
 
@@ -55,32 +91,51 @@ CREATE TABLE `feeding_fees` (
 -- Table structure for table `mark`
 --
 
-CREATE TABLE `mark` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `mark` (
   `registration_number` varchar(20) NOT NULL,
   `full_name` varchar(100) NOT NULL,
   `Class` varchar(20) NOT NULL,
-  `subjects` varchar(40) NOT NULL,
+  `subjects` varchar(100) NOT NULL,
   `term` varchar(20) NOT NULL,
   `mark_type` varchar(20) NOT NULL,
-  `date` varchar(20) NOT NULL
+  `date` varchar(20) NOT NULL,
+  `grade` varchar(50) NOT NULL,
+  `marks` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Truncate table before insert `mark`
+--
+
+TRUNCATE TABLE `mark`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `school_fees`
 --
 
-CREATE TABLE `school_fees` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `school_fees` (
   `full_name` varchar(100) NOT NULL,
   `Class` varchar(20) NOT NULL,
   `term` varchar(10) NOT NULL,
   `amount_paid` varchar(20) NOT NULL,
   `paid_by` varchar(100) NOT NULL,
-  `date` varchar(20) NOT NULL
+  `date` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Truncate table before insert `school_fees`
+--
+
+TRUNCATE TABLE `school_fees`;
+--
+-- Dumping data for table `school_fees`
+--
+
+INSERT DELAYED IGNORE INTO `school_fees` (`full_name`, `Class`, `term`, `amount_paid`, `paid_by`, `date`) VALUES
+('Ahortu', 'JHS 1', 'Term 1', '122', 'Derrick', '12th June,2022'),
+('Kofi Ana', 'JHS 1', 'Term 1', '122', 'Derrick', '12th June,2022'),
+('Hellary Ama', 'JHS 2', 'Term 1', '1228', 'John Kuwasi', '13thJune');
 
 -- --------------------------------------------------------
 
@@ -88,88 +143,25 @@ CREATE TABLE `school_fees` (
 -- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `students` (
   `registration_number` varchar(20) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
   `date_of_birth` varchar(20) NOT NULL,
   `Class` varchar(50) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `home_address` varchar(100) NOT NULL,
   `parent_full_name` varchar(100) NOT NULL,
   `phone_number` varchar(100) NOT NULL,
-  `email_address` varchar(100) NOT NULL
+  `email_address` varchar(100) NOT NULL,
+  UNIQUE KEY `registration_number` (`registration_number`),
+  UNIQUE KEY `full_name` (`full_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Truncate table before insert `students`
 --
 
---
--- Indexes for table `attendance`
---
-ALTER TABLE `attendance`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `feeding_fees`
---
-ALTER TABLE `feeding_fees`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `mark`
---
-ALTER TABLE `mark`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `school_fees`
---
-ALTER TABLE `school_fees`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `attendance`
---
-ALTER TABLE `attendance`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `feeding_fees`
---
-ALTER TABLE `feeding_fees`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `mark`
---
-ALTER TABLE `mark`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `school_fees`
---
-ALTER TABLE `school_fees`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `students`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-COMMIT;
+TRUNCATE TABLE `students`;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
