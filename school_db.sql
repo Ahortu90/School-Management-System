cc4d6119086067e4ab7e517808b70cd30aa26551
Ahortu90 <ahortuderrick4@gmail.com>
2022-10-05 11:23:00 +0000

    update

--- school_db.sql
+++ school_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2022 at 03:29 AM
+-- Generation Time: Oct 05, 2022 at 01:16 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -29,6 +29,7 @@ USE `school_db`;
 -- Table structure for table `attendance`
 --
 
+DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE IF NOT EXISTS `attendance` (
   `registration_number` varchar(20) NOT NULL,
   `full_name` varchar(100) NOT NULL,
@@ -37,34 +38,42 @@ CREATE TABLE IF NOT EXISTS `attendance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `attendance`
+-- Dumping data for table `attendance`
 --
 
-TRUNCATE TABLE `attendance`;
+INSERT INTO `attendance` (`registration_number`, `full_name`, `Class`, `date`) VALUES
+('REG-15004', 'Ahortu Derrick', 'JHS 1', '03/10/2022'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', '03/02/2022');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `feeding_fees`
 --
 
+DROP TABLE IF EXISTS `feeding_fees`;
 CREATE TABLE IF NOT EXISTS `feeding_fees` (
   `full_name` varchar(100) NOT NULL,
   `Class` varchar(20) NOT NULL,
+  `type_of_fees` varchar(50) NOT NULL,
   `amount_paid` varchar(20) NOT NULL,
   `date` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `feeding_fees`
+-- Dumping data for table `feeding_fees`
 --
 
-TRUNCATE TABLE `feeding_fees`;
+INSERT INTO `feeding_fees` (`full_name`, `Class`, `type_of_fees`, `amount_paid`, `date`) VALUES
+('ahortu', 'Class 6', 'Feeding Fees', '10', '12');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `login_info`
 --
 
+DROP TABLE IF EXISTS `login_info`;
 CREATE TABLE IF NOT EXISTS `login_info` (
   `username` varchar(20) NOT NULL,
   `fullname` varchar(100) NOT NULL,
@@ -74,15 +83,10 @@ CREATE TABLE IF NOT EXISTS `login_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `login_info`
---
-
-TRUNCATE TABLE `login_info`;
---
 -- Dumping data for table `login_info`
 --
 
-INSERT DELAYED IGNORE INTO `login_info` (`username`, `fullname`, `password`, `confirm_password`) VALUES
+INSERT INTO `login_info` (`username`, `fullname`, `password`, `confirm_password`) VALUES
 ('as', 'ahortu', '12', '12');
 
 -- --------------------------------------------------------
@@ -91,6 +95,7 @@ INSERT DELAYED IGNORE INTO `login_info` (`username`, `fullname`, `password`, `co
 -- Table structure for table `mark`
 --
 
+DROP TABLE IF EXISTS `mark`;
 CREATE TABLE IF NOT EXISTS `mark` (
   `registration_number` varchar(20) NOT NULL,
   `full_name` varchar(100) NOT NULL,
@@ -104,16 +109,28 @@ CREATE TABLE IF NOT EXISTS `mark` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `mark`
+-- Dumping data for table `mark`
 --
 
-TRUNCATE TABLE `mark`;
+INSERT INTO `mark` (`registration_number`, `full_name`, `Class`, `subjects`, `term`, `mark_type`, `date`, `grade`, `marks`) VALUES
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Enlish Language', 'Term 1', 'Exams', '03/10/2022', '1', '90'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Mathematics', 'Term 1', 'Exams', '03/10/2022', '1', '95'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Intergrated Science', 'Term 1', 'Exams', '03/10/2022', '1', '89'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Information Communication and Tech.', 'Term 1', 'Exams', '03/10/2022', '1', '98'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Social Studies', 'Term 1', 'Exams', '03/10/2022', '1', '95'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'History', 'Term 1', 'Exams', '03/10/2022', '1', '95'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'French', 'Term 1', 'Exams', '03/10/2022', '1', '70'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Basic Design and Tech.', 'Term 1', 'Exams', '03/10/2022', '3', '70'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Religious and Moral Education', 'Term 1', 'Exams', '03/10/2022', '2', '87'),
+('REG-15004', 'Ahortu Derrick', 'JHS 1', 'Ghanaian Language', 'Term 1', 'Exams', '03/10/2022', '2', '87');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `school_fees`
 --
 
+DROP TABLE IF EXISTS `school_fees`;
 CREATE TABLE IF NOT EXISTS `school_fees` (
   `full_name` varchar(100) NOT NULL,
   `Class` varchar(20) NOT NULL,
@@ -124,18 +141,15 @@ CREATE TABLE IF NOT EXISTS `school_fees` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `school_fees`
---
-
-TRUNCATE TABLE `school_fees`;
---
 -- Dumping data for table `school_fees`
 --
 
-INSERT DELAYED IGNORE INTO `school_fees` (`full_name`, `Class`, `term`, `amount_paid`, `paid_by`, `date`) VALUES
+INSERT INTO `school_fees` (`full_name`, `Class`, `term`, `amount_paid`, `paid_by`, `date`) VALUES
 ('Ahortu', 'JHS 1', 'Term 1', '122', 'Derrick', '12th June,2022'),
 ('Kofi Ana', 'JHS 1', 'Term 1', '122', 'Derrick', '12th June,2022'),
-('Hellary Ama', 'JHS 2', 'Term 1', '1228', 'John Kuwasi', '13thJune');
+('ahortu', 'JHS 3', 'Term 1', '400', 'ahortu', '13/09/2022'),
+('ahortu', 'JHS 3', 'Term 2', '400', 'ahortu', '13/09/2022'),
+('Ahortu Derrick', 'JHS 1', 'Term 1', '500', 'Derrick', '03/10/2022');
 
 -- --------------------------------------------------------
 
@@ -143,6 +157,7 @@ INSERT DELAYED IGNORE INTO `school_fees` (`full_name`, `Class`, `term`, `amount_
 -- Table structure for table `students`
 --
 
+DROP TABLE IF EXISTS `students`;
 CREATE TABLE IF NOT EXISTS `students` (
   `registration_number` varchar(20) NOT NULL,
   `full_name` varchar(100) NOT NULL,
@@ -158,10 +173,38 @@ CREATE TABLE IF NOT EXISTS `students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `students`
+-- Dumping data for table `students`
 --
 
-TRUNCATE TABLE `students`;COMMIT;
+INSERT INTO `students` (`registration_number`, `full_name`, `date_of_birth`, `Class`, `gender`, `home_address`, `parent_full_name`, `phone_number`, `email_address`) VALUES
+('REG-15004', 'Ahortu Derrick', '24/02/2022', 'JHS 1', 'Male', 'Ashiaman Lebanon Zone4, Accra, Ghana', 'Derrick', '0558039093', 'ahortuderrick0@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studies_fees`
+--
+
+DROP TABLE IF EXISTS `studies_fees`;
+CREATE TABLE IF NOT EXISTS `studies_fees` (
+  `full_name` varchar(100) NOT NULL,
+  `Class` varchar(50) NOT NULL,
+  `type_of_fees` varchar(50) NOT NULL,
+  `amount_paid` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `studies_fees`
+--
+
+INSERT INTO `studies_fees` (`full_name`, `Class`, `type_of_fees`, `amount_paid`, `date`) VALUES
+('ahortu', 'Class 6', 'Studies Fees', '10', '12'),
+('Ahortu Derrick', 'JHS 1', 'Studies Fees', '200', '13'),
+('Ahortu Derrick', 'Class 1', 'Studies Fees', '100', '03/10/2022'),
+('Ahortu Derrick', 'JHS 1', 'Studies Fees', '400', '03/10/2022'),
+('Ahortu Derrick', 'JHS 1', 'Studies Fees', '100', '03/10/2022');
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
